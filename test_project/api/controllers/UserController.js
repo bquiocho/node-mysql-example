4b74e814aa9f414b69bae06a5449af5102061ad2
Brandon Quiocho <bquiocho@tietronix.com>
2016-07-15 14:58:59 -0500

    Added a temporary remove function for deleting users from the database.

--- test_project/api/controllers/UserController.js
+++ test_project/api/controllers/UserController.js
@@ -6,7 +6,7 @@
  */
 
 module.exports = {
-	// localhost:1337/User/add will add the var to the db
+	// localhost:1337/User/add
 	add: function(req, res){
 	//var new_user = {first_name: 'Derek',last_name : 'Jeter',org : 'NYY'};
 	// User.create(new_user).exec(function(err, result){
@@ -19,34 +19,53 @@ module.exports = {
 	// });
 
 	// check if first name or last name is valid
-	if (req.body.first_name === "" || req.body.last_name === ""){
-		console.log("First name or last name not valid");
-		res.status(400).send({error: 'Must include a valid first name and last name'});
-	}
-	else{
-		var userObjToCreate = new Object();
-		userObjToCreate.first_name = req.body.first_name;
-		userObjToCreate.last_name = req.body.last_name;
-		userObjToCreate.org = req.body.org;
-
-		console.log("req.body.first_name: " + userObjToCreate.first_name);
-		console.log("req.body.last_name: " + userObjToCreate.last_name);
-		console.log("req.body.org: " + userObjToCreate.org);
-
-		var res_json = "Successfully added: "+userObjToCreate.first_name+" "+userObjToCreate.last_name +" to the database";
-		console.log(res_json);
-
-		User.create(userObjToCreate).exec(function(err, result){
-		if (err) {
-			sails.log.debug('Some error occured ' + err);
-			return res.json(500, { error: 'Some error occured' });
+		if (req.body.first_name === "" || req.body.last_name === ""){
+			console.log("First name or last name not valid");
+			res.status(400).send({error: 'Must include a valid first name and last name'});
 		}
-		sails.log.debug('Success, result: ', JSON.stringify(result));
-		return res.json(200, { success: res_json });
-	});
-}
+		else{
+			var userObjToCreate = new Object();
+			userObjToCreate.first_name = req.body.first_name;
+			userObjToCreate.last_name = req.body.last_name;
+			userObjToCreate.org = req.body.org;
 
+			console.log("req.body.first_name: " + userObjToCreate.first_name);
+			console.log("req.body.last_name: " + userObjToCreate.last_name);
+			console.log("req.body.org: " + userObjToCreate.org);
 
-}
+			var res_json = "Successfully added: "+userObjToCreate.first_name+" "+userObjToCreate.last_name +" to the database";
+			console.log(res_json);
+
+			User.create(userObjToCreate).exec(function(err, result){
+				if (err) {
+					sails.log.debug('Some error occured ' + err);
+					return res.json(500, { error: 'Some error occured' });
+				}
+				sails.log.debug('Success, result: ', JSON.stringify(result));
+				return res.json(200, { success: res_json });
+			});
+		}
+	},
+
+	// localhost:1337/User/remove will delete the user from the db
+	remove: function(req, res){
+		var first_name = req.body.first_name;
+		sails.log("Try to remove user named " + first_name);
+		var query = new Object();
+		query.first_name = first_name;
+		User.destroy(query).then(function(user) {
+			if(user.length > 0) {
+				req.flash('success', 'Successfully removed the User.');
+				//res.redirect('/console/teacher');
+			} else {
+				req.flash('error', 'Could not remove the user.');
+				//res.redirect('/console/teacher');
+			}
+		}).catch(function (err) {
+			req.flash('error', 'Could not remove the user.');
+			//res.redirect('/console/teacher');
+		});
+
+	}
 
 };
